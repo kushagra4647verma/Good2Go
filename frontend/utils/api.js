adc6be90266c504457f67418d71d814ae7f2006f
Kushagra4647verma <kushagra4647@gmail.com>
2025-08-12 20:01:51 +0530

    Corrected Errors

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5001/api" });
+const API = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://good2go.onrender.com/api"
+      : "http://localhost:5001/api",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
