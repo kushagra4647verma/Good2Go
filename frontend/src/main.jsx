0f79af4bfb91a23826fb0712df58a5208c94dc2f
Kushagra Verma <kushagraverma@Kushagras-MacBook-Air.local>
2025-08-12 19:32:14 +0530

    Errors removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+
 import { Provider } from "./components/ui/provider";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <Provider>
-    <App />
-  </Provider>
-);
+createRoot(document.getElementById("root")).render(<App />);
